868fa27355a34f72dfb92b75d538cddb11bef9b7
MG5-ZBOOK\MG5 <tnkytnk@gmail.com>
2018-01-15 22:50:57 +0900

    [Improve] more generally, Messenger set DialogResult before close.

---
+++ WpfApp1/Messenger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interactivity;
+
+namespace WpfApp1
+{
+    public class Messenger
+    {
+        public event EventHandler<MessengerEventArgs> MessengerEvent;
+        public void RaiseMessengerEvent(bool result)
+        {
+            MessengerEvent?.Invoke(this, new MessengerEventArgs(result));
+        }
+    }
+
+    public class MessengerEventArgs:EventArgs
+    {
+        public bool Result { private set; get; }
+        public MessengerEventArgs(bool result) { Result = result; }
+    }
+
+    public class MessengerEventTrigger:EventTrigger
+    {
+        protected override string GetEventName()
+        {
+            return "MessengerEvent";
+        }
+    }
+
+    public class MessengerAction : TriggerAction<System.Windows.Window>
+    {
+        protected override void Invoke(object parameter)
+        {
+            AssociatedObject.DialogResult = (parameter as MessengerEventArgs).Result;
+            AssociatedObject.Close();
+        }
+    }
+}
--- WpfApp1/ViewModel.cs
+++ WpfApp1/ViewModel.cs
@@ -25,7 +25,18 @@ namespace WpfApp1
 
             public void Execute(object parameter)
             {
-                viewModel.Close();
+                var command = (string)parameter;
+                switch (command)
+                {
+                    case "exitWnd":
+                        viewModel.Close();
+                        break;
+                    case "openDlg":
+                        var wnd = new Window1();
+                        var result = wnd.ShowDialog();
+                        viewModel.DialogResult = result.ToString();
+                        break;
+                }
             }
         }
         public ExitCommand MyExit { private set; get; }
@@ -42,6 +53,13 @@ namespace WpfApp1
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+            string dialogResult;
+            public string DialogResult
+            {
+                set { if (dialogResult != value) { dialogResult = value;PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DialogResult))); } }
+                get { return dialogResult; }
+            }
+            
         public bool Exitable { private set; get; }
         public DateTime ClockTime { private set; get; }
         DispatcherTimer dispatcherTimer;
@@ -69,6 +87,48 @@ namespace WpfApp1
             dispatcherTimer.Tick -= DispatcherTimer_Tick;
 
             MyCloseViewMessenger.RaiseViewModelCloseEvent();
+        }
+    }
+
+    class DialogViewModel : INotifyPropertyChanged
+    {
+        public class DialogButtonCommand : ICommand
+        {
+            DialogViewModel parent;
+            public DialogButtonCommand(DialogViewModel parent) { this.parent = parent; }
+
+            public event EventHandler CanExecuteChanged;
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object parameter)
+            {
+                var command = (string)parameter;
+                switch(command)
+                {
+                    case "cancelCmd":
+                        parent.MyMessenger.RaiseMessengerEvent(false);
+                        break;
+                    case "confirmCmd":
+                        parent.MyMessenger.RaiseMessengerEvent(true);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        public DialogButtonCommand MyButtonCommand { private set; get; }
+
+        public Messenger MyMessenger { private set; get; } = new Messenger();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DialogViewModel()
+        {
+            MyButtonCommand = new DialogButtonCommand(this);
         }
 
     }
